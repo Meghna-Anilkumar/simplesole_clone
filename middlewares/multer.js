5764fcdf3f927bac192b9571dc57d9c355bc093b
Meghna <meghnaanil98@gmail.com>
2025-07-22 00:50:13 +0530

    moved uploads to public

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -3,7 +3,7 @@ const multer = require('multer')
 //image upload
 var storage = multer.diskStorage({
   destination: function (req, res, cb) {
-    cb(null, './uploads')
+    cb(null, './public/uploads')
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname)
